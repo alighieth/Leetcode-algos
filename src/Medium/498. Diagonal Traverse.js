5b26d8cc93c2e7a4acfd1270f1efed85705eee4c
Ali Ghieth <alighieth2709@gmail.com>
2024-06-24 02:13:44 +0300

    Create 498. Diagonal Traverse.js

---
+++ src/Medium/498. Diagonal Traverse.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var findDiagonalOrder = function (mat) {
+  const printArr = [];
+  let x = 0;
+  let y = 0;
+
+  let counter = 0;
+  let goingUp = true;
+
+  const rowSize = mat.length;
+  const colSize = mat[0].length;
+
+  while (printArr.length < rowSize * colSize) {
+    if (goingUp) {
+      while (x >= 0 && y < colSize) {
+        printArr.push(mat[x][y]);
+        x--;
+        y++;
+      }
+
+      if (y >= colSize) {
+        x -= 2;
+        y--;
+      } else {
+        x--;
+        y++;
+      }
+      goingUp = false;
+    } else {
+      while (x < rowSize && y >= 0) {
+        printArr.push(mat[x][y]);
+        x++;
+        y--;
+      }
+
+      if (y < 0) {
+        y += 2;
+        x--;
+      } else {
+        x++;
+        y--;
+      }
+    }
+    goingUp = true;
+  }
+
+  return printArr;
+};
