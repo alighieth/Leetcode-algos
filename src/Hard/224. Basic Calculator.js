760856f9fd9bd991021b68d542ee114d46067f23
Ali Ghieth <alighieth2709@gmail.com>
2024-06-29 04:36:41 +0300

    Create 224. Basic Calculator.js

---
+++ src/Hard/224. Basic Calculator.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function (s) {
+  s = s.replace(/\s/g, "");
+  const sSplit = s.split("");
+  let globalPointer = 0;
+
+  return handleIsolation();
+
+  function handleIsolation() {
+    const res = [];
+
+    let preOperator = "+";
+    let num = "";
+
+    while (sSplit[globalPointer] != ")" && globalPointer < sSplit.length) {
+      const element = sSplit[globalPointer];
+
+      const parsedElement = parseInt(element);
+      if (!isNaN(parsedElement)) num += element;
+      else if (["+", "-"].includes(element)) {
+        const parsedNum = parseInt(num);
+        if (preOperator === "+" && String(num).length > 0)
+          res.push(1 * parsedNum);
+        if (preOperator === "-" && String(num).length > 0)
+          res.push(-1 * parsedNum);
+        preOperator = element;
+        num = "";
+      } else {
+        // ( or )
+        if (element === "(") {
+          globalPointer++;
+          num = String(handleIsolation());
+        }
+      }
+
+      globalPointer++;
+    }
+    if (num.length > 0) {
+      const parsedNum = parseInt(num);
+      res.push((preOperator === "-" ? -1 : 1) * parsedNum);
+    }
+    return res.reduce((acc, curr) => acc + parseInt(curr), 0);
+  }
+};
